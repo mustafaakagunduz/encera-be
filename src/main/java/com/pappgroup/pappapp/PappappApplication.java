e360ba882d91bed52c87cd23779879d5eda7b19a
Mustafa Akagündüz <akagunduzmustafa00@gmail.com>
2025-07-02 19:03:59 +0300

    first commit

---
+++ src/main/java/com/pappgroup/pappapp/PappappApplication.java
@@ -0,0 +1,13 @@
+package com.pappgroup.pappapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PappappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PappappApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pappgroup/pappapp/PappappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pappgroup.pappapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PappappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
